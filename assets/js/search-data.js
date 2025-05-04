7205f509411150ce5887dc9762736798b0d8ee55
chenchen-usyd <128677012+chenchen-usyd@users.noreply.github.com>
2025-05-04 05:22:17 +0000

    Deploying to gh-pages from @ chenchen-usyd/chenchen-usyd.github.io@77bef55e9ac9141b48eeaf9d5316ecef0d1fa086 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-have-completed-my-applied-scientist-internship-at-amazon-and-received-an-inclined-vote-for-return",
+          title: 'I have completed my Applied Scientist internship at Amazon and received an inclined...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
