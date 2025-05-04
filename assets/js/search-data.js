77dc4d7b184ba11c2f24058a6c5df0447fa1c279
chenchen-usyd <128677012+chenchen-usyd@users.noreply.github.com>
2025-05-04 08:37:31 +0000

    Deploying to gh-pages from @ chenchen-usyd/chenchen-usyd.github.io@69561321d2627a50e370ae673e12098ee7e1b51e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,19 +16,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "I am both a tutor and a guest lecturer in the School of Computer Science at the University of Sydney.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
           },
         },{id: "post-a-post-with-plotly-js",
         
